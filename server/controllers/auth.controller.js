cb54d259c86afeb5a2491e282f0a97ad4ab87873
nishant <nd221619@gmail.com>
2024-10-11 00:10:40 +0530

    refactor: folder structure

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,6 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 dotenv.config();
 
 // Login function
--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
 
